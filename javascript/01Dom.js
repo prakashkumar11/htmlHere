7d9e874e32d686f63ca34bb1e726acdf2819e408
Prakash Kumar <prakashsharma72092@gmail.com>
2024-05-05 11:19:02 +0530

    HTML Code Prepare

---
+++ javascript/01Dom.js
@@ -0,0 +1,5 @@
+// Dom Manipulation
+
+// GetElementById
+document.getElementById('main-heading');
+console.log(title);
---
+++ javascript/basic1.js
@@ -0,0 +1,13 @@
+let num = [7, 1, 9];
+let res = num.toString();
+// console.log(res);
+// console.log(typeof res);
+
+let res1 = num.join("-")
+// console.log(res1);
+
+// console.log(num.pop());
+
+let v=[19, 12, 23];
+console.log(num.push(v));
+console.log(num);
